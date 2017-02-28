dcc7d4a97aace0d08612dcc0a4f52dac5df1e4e4
YanisMat <katuhs1@gmail.com>
2017-02-28 20:29:27 +0300

    добавила слова и самонабирающийся текст
    хз откуда появился скролл, хотя везде он хидден

---
+++ js/indexword.js
@@ -0,0 +1,50 @@
+var words = [
+'экспрессия',
+'депрессия',
+'стюардессия',
+'Василисия',
+'досвидания',
+'Македония',
+'Слова закончились',
+'Придумала',
+'дедлайн',
+'Поставьте зачет',
+'пожалуйста'
+];
+
+function randomWord() {
+  var randWord = words[Math.floor(Math.random() * words.length)];
+  return $('#result').text(randWord);
+}
+
+// Generate first random word
+randomWord();
+
+// When KeyDown
+$(document).bind('keydown', function() {
+  $('#result').addClass('active');
+});
+
+// When KeyUp
+$(document).bind('keyup', function() {
+  $('#result').removeClass('active');
+  randomWord();
+});
+
+// When ClickDown
+$('#result').bind('mousedown', function() {
+  $('#result').addClass('active');
+});
+
+// When ClickUp
+$('#result').bind('mouseup', function() {
+  $('#result').removeClass('active');
+  randomWord();
+});
+
+// Change Color
+$('#changecolor').click(function() {
+  $('#container').toggleClass('black', 'white');
+  $('#changecolor').toggleClass('cblack', 'cwhite');
+  $(this).blur();
+});
